c04b02d9bfc02be1cdbf38dafeb07716c5460fd5
Abdelmajid EL HOU <abdelmajid.elhou@gmail.com>
2024-03-11 17:14:41 +0100

    merge

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from scripts import excel_checker
 from scripts import url_finder
 import pandas as pd
 
+def chunk_dataframe(df, chunk_size):
+    chunks = []
+    num_chunks = len(df) // chunk_size + 1
+    for i in range(num_chunks):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size
+        chunks.append(df[start:end])
+    return chunks
+
 def main():
     config_file_path = "conf/config.json"  # Chemin vers votre fichier de configuration
+    #chunk_size = 10  # Taille des chunks pour le découpage du DataFrame
 
     try:
         config_data = excel_checker.load_config(config_file_path)
@@ -17,14 +27,21 @@ def main():
             if 'LinkedIn' not in data.columns:
                 data['LinkedIn'] = None  # Si les colonnes LinkedIn ne sont pas présentes, les initialiser avec None
             
-            # Appel de la fonction pour ajouter les liens LinkedIn
-            df_with_links = url_finder.generate_linkedin_urls(data)
+            # Diviser le DataFrame en petits sous-DataFrames
+            chunk_size = config_data.get("chunk_size")
+            chunks = chunk_dataframe(data, chunk_size)
+            
+            # Appliquer la fonction sur chaque sous-DataFrame et concaténer les résultats
+            result_frames = []
+            for chunk in chunks:
+                result_frames.append(url_finder.generate_linkedin_urls(chunk))
+            df_with_links = pd.concat(result_frames)
             
             # Afficher le DataFrame avec les liens LinkedIn
             print(df_with_links)
             
             # Enregistrer le DataFrame avec les liens LinkedIn dans un fichier Excel
-            output_file_path = "res/linkedin_links.xlsx"  # Chemin vers le fichier de sortie
+            output_file_path = config_data.get("output_file_path")
             df_with_links.to_excel(output_file_path, index=False)
             print(f"Les résultats ont été enregistrés dans : {output_file_path}")
         else:
