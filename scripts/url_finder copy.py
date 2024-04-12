96eb013b7414aca7fd886d27ddf6f7878599a5aa
Abdelmajid EL HOU <abdelmajid.elhou@gmail.com>
2024-04-12 11:28:36 +0200

    use request instead of googlesearch api

--- scripts/url_finder copy.py
+++ scripts/url_finder copy.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import requests
+from googlesearch import search
 import time
+from urllib.error import HTTPError
 
 def find_linkedin_url(nom, fonction, plateforme_ou_pole, entreprise, max_retries=3):
     queries = [
@@ -12,18 +13,11 @@ def find_linkedin_url(nom, fonction, plateforme_ou_pole, entreprise, max_retries
     for query in queries:
         for _ in range(max_retries):
             try:
-                headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-                }
-                url = 'https://www.google.com/search?q=' + query
-                res = requests.get(url, headers=headers)
-                res.raise_for_status()  # Raise an exception for HTTP errors
-                links = res.text.split('"')
-                for link in links:
-                    if 'linkedin.com/in' in link:
-                        return link
-            except requests.HTTPError as e:
-                if e.response.status_code == 429:
+                for url in search(query, num=1, stop=1):
+                    if 'linkedin.com/in' in url:
+                        return url
+            except HTTPError as e:
+                if e.code == 429:
                     print(f"Erreur 429: Trop de requêtes. Attente avant de réessayer...")
                     time.sleep(60)  # Attendre 60 secondes avant de réessayer
                 else:
@@ -34,6 +28,8 @@ def find_linkedin_url(nom, fonction, plateforme_ou_pole, entreprise, max_retries
                 print("Réessai dans quelques secondes...")
                 time.sleep(5)  # Attendre quelques secondes avant de réessayer
     return None
+
+
 
 def generate_linkedin_urls(data):
     if isinstance(data, pd.DataFrame):
